ffefed9edea4f45bf00b6fce5c85ca397d14f564
Lorna <lorna1661maria@gmail.com>
2023-01-25 14:59:45 -0500

    adds functions in src files

--- src/dbcon.py
+++ src/dbcon.py
@@ -2,10 +2,9 @@
 Script to connect to cloud storage (Azure)
 """
 
+
 def azureconn():
-    """
-    connects to azure and returns file
-    """
+    """connects to azure and returns file"""
 
     file = 4
     return file
--- src/extract.py
+++ src/extract.py
@@ -2,14 +2,19 @@
 Function to read file into application based on file type
 """
 import pandas as pd
-from dbcon import file
+
+# from dbcon import file
+
 
 def find_file_type():
-    """ Detect file type"""
+    """Detect file type"""
     filetype = "csv"
     return filetype
 
+
 def create_data_object(filetype):
     """Created data object depending on file type"""
+    if filetype == "csv":
+        data_object = pd.read_csv(filetype)
 
-    return DataObject
+    return data_object
