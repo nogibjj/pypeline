8b0ac843e2d45e3d66f14a0a2b4b375b4fa2f3a7
Lorna <lorna1661maria@gmail.com>
2023-01-31 23:34:34 -0500

    adjust api endpoints

--- app/api.py
+++ app/api.py
@@ -1,9 +1,11 @@
 """
 Module for api requests
 """
+# import re
 from fastapi import FastAPI
 from src import read
 from src import transform
+
 
 app = FastAPI()
 
@@ -15,6 +17,13 @@ async def create_pipeline(file: str):
     """function to get user data"""
     data = read.create_data_object(file)
     data_hold.append(data)
+    return {"message": "Data successfully uploaded"}
+
+
+@app.get("/summary")
+async def data_summary():
+    """function to create data summary"""
+    data = data_hold[0]
     load_summary = transform.create_data_report(data)
     return load_summary
 
@@ -23,12 +32,14 @@ async def create_pipeline(file: str):
 async def choose_transformation(task: str):
     """function to choose transformation to apply to data"""
     data = data_hold[0]
+    # match = re.search(r"\bmissing\b", task)
     if task == "missing":
         if task.endswith("all"):
             clean = transform.remove_missing(data, "all")
         else:
             clean = transform.remove_missing(data)
     else:
+        # match = re.search(r"\bduplicate\b", task)
         col = task[2]
         if task.endswith("last"):
             clean = transform.remove_dups(data, col, "last")
